31ecb8475dcd10fe450552c4d5a0b6c08b682a37
n1kFrost <frost3297@gmail.com>
2023-08-30 19:39:21 +0500

    feat: header

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,7 @@
 import { AppWrapper } from "./styles.js";
-
+import { Header } from "../features/Header/index.jsx";
 function App() {
-  return <AppWrapper>Layout</AppWrapper>;
+  return <AppWrapper><Header /></AppWrapper>;
 }
 
 export default App;
